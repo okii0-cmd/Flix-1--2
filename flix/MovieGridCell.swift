baf92cf5f6fa9ad807a77d7f49571f19d50570ac
okii0-cmd <63493883+okii0-cmd@users.noreply.github.com>
2021-10-02 18:01:17 -0400

    Initial commit

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by okii on 2/10/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
